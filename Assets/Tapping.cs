3d357df8af6dacd7064f97f87117b9c70c85b010
Tobi <toblerus@hotmail.de>
2018-05-17 22:26:10 +0200

    Simple Text Layout
    No function at all yet

--- Assets/Tapping.cs
+++ Assets/Tapping.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Tapping : MonoBehaviour {
     [SerializeField] float logs;
     [SerializeField] float maxLogs;
     [SerializeField] float cashearnings;
     [SerializeField] float currentcash;
+    [SerializeField] int hits;
+    public Text hitsText;
+    [SerializeField] public Button HitButton;
 
-	// Use this for initialization
-	void Start () {
-		
+
+    void Start ()
+    {
+        Button HitButton = HitButton.GetComponent<Button>();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public void treehit()
+    {
+        hits += 1;
+        hitsText.text = "Hits: " + hits.ToString();
+
+    }
 }
