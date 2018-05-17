2f9b16746d3bcc92300a5e6adce24c7859617ef6
Tobi <toblerus@hotmail.de>
2018-05-17 09:04:59 +0200

    Initial commit
    Added Scene and first basic script

--- Assets/Tapping.cs
+++ Assets/Tapping.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tapping : MonoBehaviour {
+    [SerializeField] float logs;
+    [SerializeField] float maxLogs;
+    [SerializeField] float cashearnings;
+    [SerializeField] float currentcash;
 
 	// Use this for initialization
 	void Start () {
