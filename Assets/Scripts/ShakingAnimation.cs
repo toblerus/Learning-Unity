2349e6d98585441eefa5ea59f165794e118fe6ee
Tobi <toblerus@hotmail.de>
2018-05-25 19:30:26 +0200

    DOTween introduction

---
+++ Assets/Scripts/ShakingAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShakingAnimation : MonoBehaviour {
+
+    [SerializeField] [Range(0,1)] public float duration;
+    [SerializeField] [Range(0, 1)] public float strength;
+    [SerializeField] [Range(0, 20)] public int leafAmount;
+
+    [SerializeField] private ParticleSystem leafParticleSystem;
+
+    [SerializeField] private Transform treeTransform;
+
+    public void TreeImpactAnimation()
+    {
+        //treeTransform.DOShakePosition(duration, strength, vibrato, randomness);
+        treeTransform.DOKill(true);
+        treeTransform.DOPunchPosition(Vector3.up * strength, duration);
+        leafParticleSystem.Emit(Random.Range(leafAmount, leafAmount + 5));
+    }
+}
